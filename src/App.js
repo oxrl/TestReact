2580968241173a37aee74fc3ff38851362d52c9f
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2018-12-09 21:53:37 -0500

    I added the presentation of the tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { todos } from './todos.json';
 
 class App extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+          todos
+        }
+    }
+
+
   render() {
+    const todos = this.state.todos.map((todo,i) => {
+        return (
+           <div className="col-md-4">
+                <div className="card mt-4">
+                    <div className="card-header">
+                        <h3>{todo.title}</h3>
+                        <span className="badge badge-pill badge-danger ml-2">
+                            {todo.priority}
+                        </span>
+                    </div>
+                    <div className="card-body">
+                        <p>{todo.description}</p>
+                        <p><mark>{todo.responsible}</mark></p>
+                    </div>
+                </div>
+           </div>
+        )
+      })
+
+
     return (
       <div className="App">
-        <header className="App-header">
+          <nav className="navbar navbar-dark bg-dark">
+              <a className="navbar-brand" href="/">
+                Task
+                   <span className="badge badge-pill badge-light ml-2">
+                      {this.state.todos.length}
+                   </span>
+              </a>
+          </nav>
+          <div className="container">
+              <div className="row mt-5">
+                  {todos}
+              </div>
+          </div>
+
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
