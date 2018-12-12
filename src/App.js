ab8e7cea8f5f762ed6bd308a84402c8494b4ec59
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2018-12-12 02:41:05 -0500

    Se agrego las funcionalidades de añadir tareas y eliminar tareas

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { todos } from './todos.json';
-
+// subcomponents
+import TodoForm from './components/TodoForm';
 class App extends Component {
 
     constructor(){
@@ -10,13 +11,26 @@ class App extends Component {
         this.state = {
           todos
         }
+        this.handleAddTodo = this.handleAddTodo.bind(this);
+    }
+    removeTodo(index) {
+        this.setState({
+            todos: this.state.todos.filter((e, i) => {
+                return i !== index
+            })
+        });
     }
 
+    handleAddTodo(todo) {
+        this.setState({
+            todos: [...this.state.todos, todo]
+        })
+    }
 
   render() {
     const todos = this.state.todos.map((todo,i) => {
         return (
-           <div className="col-md-4">
+           <div className="col-md-4" key={i}>
                 <div className="card mt-4">
                     <div className="card-header">
                         <h3>{todo.title}</h3>
@@ -27,6 +41,13 @@ class App extends Component {
                     <div className="card-body">
                         <p>{todo.description}</p>
                         <p><mark>{todo.responsible}</mark></p>
+                    </div>
+                    <div className="card-footer">
+                        <button
+                            className="btn btn-danger"
+                            onClick={this.removeTodo.bind(this, i)}>
+                            Delete
+                        </button>
                     </div>
                 </div>
            </div>
@@ -45,12 +66,20 @@ class App extends Component {
               </a>
           </nav>
           <div className="container">
-              <div className="row mt-5">
-                  {todos}
+              <div className="row mt-4">
+
+                  <div className="col-md-4 text-center">
+                      <img src={logo} className="App-logo" alt="logo" />
+                      <TodoForm onAddTodo={this.handleAddTodo}></TodoForm>
+                  </div>
+
+                  <div className="col-md-8">
+                      <div className="row">
+                          {todos}
+                      </div>
+                  </div>
               </div>
           </div>
-
-          <img src={logo} className="App-logo" alt="logo" />
 
       </div>
     );
